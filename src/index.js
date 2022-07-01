356c2a62fb3073b0a465b706438b26ce4b1f2a53
yugamxps <yugam.xportsoft@gmail.com>
2022-07-01 16:10:43 +0530

    nested components & jsx css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-function Greeting() {
-  return <h4>this is yugam and this is my first component</h4>;
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Amelia Hepworth
+  </h4>
+);
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
