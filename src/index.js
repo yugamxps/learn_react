3a40d2e31641ae9a4e84317a98894de743bad6fa
yugamxps <yugam.xportsoft@gmail.com>
2022-07-04 15:15:04 +0530

    till children props

--- src/index.js
+++ src/index.js
@@ -1,48 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//CSS
 import "./index.css";
+
+//setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        author={firstBook.author}
+        title={firstBook.title}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          non commodi quod similique tempore deserunt exercitationem iusto
+          voluptatibus omnis veritatis!
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        author={secondBook.author}
+        title={secondBook.title}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, author, title, children } = props;
   return (
     <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Amelia Hepworth
-  </h4>
-);
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
